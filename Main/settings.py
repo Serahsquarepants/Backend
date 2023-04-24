4676128e2aea64672a0add986bfa1499286a16fd
Dimitar <dimitar.iliev42@gmail.com>
2023-04-24 22:14:46 +0200

    allowed_host

--- Main/settings.py
+++ Main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6t)q7u)owc2v6s5e6bq+766)aqjt+%9hl+xkn6xk(iqfv+-m9p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'libro',
-    
     'rest_framework',
+    'libro',
 ]
 
 MIDDLEWARE = [
